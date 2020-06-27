16e16c8bd04b105b6a8cc2cce06eb9dfb851d1ea
Honson Tran <honsontran@gmail.com>
2020-06-27 15:55:20 -0400

    Detection/Mask-RCNN: Add new data to multi-class spine

--- Detection/Mask-RCNN/samples/spine_segmented/spine_segmented.py
+++ Detection/Mask-RCNN/samples/spine_segmented/spine_segmented.py
@@ -27,9 +27,6 @@ Usage: import the module (see Jupyter notebooks for examples), or run from
 import tensorflow as tf
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
-tf.Session(config=config)
-
-config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.4
 tf.Session(config=config)
 
